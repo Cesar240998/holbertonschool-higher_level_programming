0f7379d989660550a2b141ee87bec5a369624aa2
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-10 00:40:22 -0500

    fixing pep

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number_str = repr(number)
-last_dig = number_str[-1]
+ns = repr(number)
+last_dig = ns[-1]
 if number > -1:
-    last_int = int(last_dig)
+    la = int(last_dig)
 else:
-    last_int = int(last_dig) * -1
+    la = int(last_dig) * -1
 
-if last_int > 5:
-    print("Last digit of {0} is {1} and is greater than 5".format(number, last_int))
-elif last_int == 0:
-    print("Last digit of {0} is {1} and is 0".format(number, last_int))
-elif last_int < 6:
-    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, last_int))
+if la > 5:
+    print("Last digit of {0} is {1} and is greater than 5".format(ns, la))
+elif la == 0:
+    print("Last digit of {0} is {1} and is 0".format(ns, la))
+elif la < 6:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(ns, la))
