ef463dc29a6a084bc25981167c80eaec5e33fbec
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-18 23:25:27 -0500

    fixed

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    for index in my_list:
-        if index > biggest:
-            biggest = index
-    return biggest
+    if (len(my_list) == 0):
+        return (None)
+    max = my_list[0]
+    for num in my_list:
+        if (num > max):
+            max = num
+    return (max)
