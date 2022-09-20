60b8f131c0ddf783adc1208c7245c3fb9a465dce
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-20 13:24:08 -0500

    fixing code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
-        return (None) 
+        return (None)
     else:
         val_dict = a_dictionary.values()
         maxim_val = max(val_dict)
