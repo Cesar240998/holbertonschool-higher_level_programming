46f4cd0d312feb65a1ea2db45b6ad7eb952bc8dd
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-23 21:00:00 -0500

    fixing pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-Square has an 1 argument: size 
+
+Square has an 1 argument: size
+
+
 """
 
+
 class Square:
-        """ 
-        Initializion size 
-        """
-        def __init__(self, size):
-            self.__size = size
+    """
+    Initializion size
+    """
+    def __init__(self, size):
+        self.__size = size
