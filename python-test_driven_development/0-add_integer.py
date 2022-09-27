9bfcd090d5173ee3d1784c56a3df479bd8c358ff
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-27 01:53:45 -0500

    task00

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,13 +8,9 @@ Returns sum of parameters
 
 def add_integer(a, b=98):
     """ Checks if int, otherwise return sum """
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-
-    if type(a) != int:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif type(b) != int:
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
-        return a + b
+        return int(a) + int(b)
