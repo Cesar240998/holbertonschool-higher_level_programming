cdff69ec063d8adc63d09569ef2ebdaa19574bd8
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-27 17:30:27 -0500

    task2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -49,4 +49,7 @@ class Rectangle:
 
     def perimeter(self):
         """Method to get the perimeter of the Rectangle"""
-        return (self.__width * 2 + self.__height * 2)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2 + self.__height * 2)
