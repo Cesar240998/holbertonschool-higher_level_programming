9616a6e88d5cb35ce15c5e0cf8bc8271d0c433d4
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-09-30 00:21:59 -0500

    task3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+        """Returns True if obj is same instance, else False
+        """
+        return isinstance(obj, a_class)
