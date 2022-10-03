9b2ade4bc72b3ac4f113e99a3ba6004024e5ee01
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-02 20:50:05 -0500

    fixing pep

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,10 +5,8 @@
 
 class BaseGeometry:
     """Contains a function area()
-    """
-       
+    """ 
     def area(self):
         """Function not implemented yet
-        """
-        
+        """ 
         raise Exception("area() is not implemented")
