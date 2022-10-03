47b6a05ed6c61f277a1f4b32da46692bc5fc387a
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-02 22:49:13 -0500

    task11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""base_geometry
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class square based in rectangle
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Method that calculates area of square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Magic method to print square description
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
