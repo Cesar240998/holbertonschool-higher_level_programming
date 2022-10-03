b3ef969a21b8766bb0214b940f050d5b8caad4c6
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-02 21:34:56 -0500

    fixing pep

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,8 +9,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """
     Class Rectangle based in BaseGeometry
-    """
-       
+    """    
     def __init__(self, width, height):
         """Constructor of Retangle"""
         BaseGeometry.integer_validator(self, 'width', width)
