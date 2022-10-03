dabdeffb37e82656ad91b0fb21e94f5e820ac445
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-02 21:33:23 -0500

    task8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""base_geometry
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle based in BaseGeometry
+    """
+       
+    def __init__(self, width, height):
+        """Constructor of Retangle"""
+        BaseGeometry.integer_validator(self, 'width', width)
+        self.__width = width
+        BaseGeometry.integer_validator(self, 'height', height)
+        self.__height = height
