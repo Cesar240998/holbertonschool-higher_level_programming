eac1790f9394c4641ac254b1f97d6575d7493ab1
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-02 20:49:07 -0500

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""base_geometry
+"""
+
+
+class BaseGeometry:
+    """Contains a function area()
+    """
+       
+    def area(self):
+        """Function not implemented yet
+        """
+        
+        raise Exception("area() is not implemented")
