e0760c0249ab1ee2ec12896ee69de1e631724969
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-06 11:42:57 -0500

    task11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Student
+"""
+
+
+class Student:
+    """Contains student data
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary of Student
+        """
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
+
+    def reload_from_json(self, json):
+        """Upadtes dictionary
+        """
+        for x in json:
+            self.__dict__.update({x: json[x]})
