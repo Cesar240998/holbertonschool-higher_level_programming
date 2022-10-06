eec1b9228b04b8a6e1796c7297860d00850963cb
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-06 01:27:00 -0500

    fixinf identation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,13 +6,14 @@ add_item
 
 import sys
 
+
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 try:
-        loadFile = load_from_json_file("add_item.json")
+    loadFile = load_from_json_file("add_item.json")
 except FileNotFoundError:
-        loadFile = []
+    loadFile = []
 
 argc = len(sys.argv)
 for idx in range(1, argc):
