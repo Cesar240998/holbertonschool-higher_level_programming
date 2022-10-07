18007a35bbca2fbf914342715bd356858e8a7764
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-07 17:05:43 -0500

    adding display with #

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,3 +71,10 @@ class Rectangle(Base):
     def area(self):
         """Calculates the area of a Rectangle instance."""
         return self.__width * self.__height
+
+    def display(self):
+        """Prints the rectangle with # char"""
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                print("#", end="")
+            print()
