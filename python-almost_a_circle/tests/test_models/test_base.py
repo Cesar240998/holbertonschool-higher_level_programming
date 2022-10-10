02a5d986a75e121c37daa2334af299ba6e010c95
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-09 21:22:26 -0500

    tests for base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -9,36 +9,34 @@ from models.square import Square
 
 
 class Test_Base(unittest.TestCase):
-    def test_Base_Id(self):
-        """This method creates a id secuential
+    def test_Id_positive(self):
+        """Testing for positive Base class ID
         """
-        b1 = Base(5)
-        b2 = Base(12)
-        self.assertEqual(b1.id, 5)
-        self.assertEqual(b2.id, 12)
+        b1 = Base(2)
+        self.assertEqual(b1.id, 2)
+        b2 = Base(7)
+        self.assertEqual(b2.id, 7)
 
-    def test_Base_Empty(self):
-        """This method creates a id Empty
+    def test_Id_negative(self):
+        """Testing for negative Base class ID
+        """
+        b1 = Base(-20)
+        self.assertEqual(b1.id, -20)
+        b2 = Base(-5)
+        self.assertEqual(b2.id, -5)
+
+    def test_Id_as_none(self):
+        """Test for None Base Class ID
         """
         b1 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base(None)
+        self.assertEqual(b2.id, 2)
 
-    def test_Base_Negative(self):
-        """This method creates a id Negative
+    def test_string_id(self):
+        """Test Id as string
         """
-        b1 = Base(-5)
-        b2 = Base(-13)
-        self.assertEqual(b1.id, -5)
-        self.assertEqual(b2.id, -13)
-
-    def test_Base_to_json_string_None(self):
-        """This method validates a parameter None
-        """
-        json_dictionary = Base.to_json_string(None)
-        self.assertEqual(json_dictionary, '[]')
-
-    def test_Base_to_json_string_empty(self):
-        """This method validates a parameter Empty
-        """
-        json_dictionary = Base.to_json_string([])
-        self.assertEqual(json_dictionary, '[]')
+        b1 = Base("Messi")
+        self.assertEqual(b1.id, "Messi")
+        b2 = Base("Cris")
+        self.assertEqual(b2.id, "Cris")
