ad2691ed6c260cf9c130b005272446b5b3510797
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-09 20:32:23 -0500

    adding more tests for Base class

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,53 +8,37 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-class TestBase(unittest.TestCase):
-    """Testing Base
-    """
-    
-    def tearDown(self):
-        """Tears down obj count
+class Test_Base(unittest.TestCase):
+    def test_Base_Id(self):
+        """This method creates a id secuential
         """
-        Base._Base__nb_objects = 0
-        self.assertEqual(Base._Base__nb_objects, 0)
+        b1 = Base(5)
+        b2 = Base(12)
+        self.assertEqual(b1.id, 5)
+        self.assertEqual(b2.id, 12)
 
-    def test_instance(self):
-        """Test instantiation
+    def test_Base_Empty(self):
+        """This method creates a id Empty
         """
         b1 = Base()
-        b2 = Base(9)
-        b3 = Base(9.5)
-        b4 = Base(float('inf'))
-        b5 = Base("string")
-        b6 = Base(["list", 4, 2.5])
-        b7 = Base(None)
-
         self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 9)
-        self.assertEqual(b3.id, 9.5)
-        self.assertEqual(b4.id, float('inf'))
-        self.assertEqual(b5.id, "string")
-        self.assertEqual(b6.id, ["list", 4, 2.5])
-        self.assertEqual(b7.id, 2)
-        self.assertEqual(Base._Base__nb_objects, 2)
 
-    def test_to_json_string(self):
-        """Testing to_json_string()
+    def test_Base_Negative(self):
+        """This method creates a id Negative
         """
-        b1_1 = [{"hi": 1, "yo": "hol"}]
-        b1_2 = [{"hello": 3}]
-        b1_3 = None
-        b1_4 = "a string"
-        b1_5 = 123
-        b1_6 = [[1, 2, 3]]
-        b1_7 = []
+        b1 = Base(-5)
+        b2 = Base(-13)
+        self.assertEqual(b1.id, -5)
+        self.assertEqual(b2.id, -13)
 
-        self.assertCountEqual(Base.to_json_string(b1_1),
-                              '[{"hi": 1, "yo": "hol"}]')
-        self.assertCountEqual(Base.to_json_string(b1_2), '[{"hello": 3}]')
-        self.assertCountEqual(Base.to_json_string(b1_3), '[]')
-        self.assertCountEqual(Base.to_json_string(b1_4), '"a string"')
-        with self.assertRaises(TypeError):
-            Base.to_json_string(b1_5)
-        self.assertCountEqual(Base.to_json_string(b1_6), '[[1, 2, 3]]')
-        self.assertCountEqual(Base.to_json_string(b1_7), '[]')
+    def test_Base_to_json_string_None(self):
+        """This method validates a parameter None
+        """
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, '[]')
+
+    def test_Base_to_json_string_empty(self):
+        """This method validates a parameter Empty
+        """
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, '[]')
