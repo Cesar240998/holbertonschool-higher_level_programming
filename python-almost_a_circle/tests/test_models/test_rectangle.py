062fbb1e0cd2e304ca3072a0bf3f6411e6d021d5
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-09 23:11:05 -0500

    tests for update

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -100,10 +100,49 @@ class Test_Rectangle_Attributes_Methods(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as fake_out:
             r2.display()
             self.assertEqual(fake_out.getvalue(), prints)
-    
+
     def test_Rectangle_display2(self):
         r3 = Rectangle(1, 2)
         prints = "#\n#\n"
         with patch('sys.stdout', new=StringIO()) as fake_out:
             r3.display()
             self.assertEqual(fake_out.getvalue(), prints)
+
+    def test_dict(self):
+        """Test to dictionary function
+        """
+        r1 = Rectangle(10, 2, 1, 9, 9)
+        r1_dict = r1.to_dictionary()
+        d = {'x': 1, 'y': 9, 'id': 9, 'height': 2, 'width': 10}
+        self.assertDictEqual(r1_dict, d)
+
+    def test_update(self):
+        """Test for public method update.
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89)
+        self.assertEqual(r1.id, 89)
+        r1.update(89, 2)
+        self.assertEqual(r1.width, 2)
+        r1.update(89, 2, 3)
+        self.assertEqual(r1.height, 3)
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(r1.x, 4)
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(r1.y, 5)
+        r1.update()
+        self.assertEqual(str(r1), "[Rectangle] (89) 4/5 - 2/3")
+
+    def test_update_kwargs(self):
+        """Test Update kwargs
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.id = 1
+        r1.update(height=1)
+        self.assertEqual(str(r1), '[Rectangle] (1) 10/10 - 10/1')
+        r1.update(width=1, x=2)
+        self.assertEqual(str(r1), '[Rectangle] (1) 2/10 - 1/1')
+        r1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(str(r1), '[Rectangle] (89) 3/1 - 2/1')
+        r1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(str(r1), '[Rectangle] (89) 1/3 - 4/2')
