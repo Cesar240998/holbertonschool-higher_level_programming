0363f56af0295c83e4b07e1d876a0836ae24810d
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-11-19 12:20:01 -0500

    fixing pycodestyleg

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+"""Lists all states with a name starting with N from a database"""
 
 if __name__ == '__main__':
     from sys import argv
