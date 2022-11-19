c094ff07002c2ec63c32e4d55402ead621d5750c
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-11-19 15:26:08 -0500

    deleting blank spaces

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -22,9 +22,9 @@ if __name__ == '__main__':
     except Exception:
         print("Can't connect to database")
         exit(0)
-    
+
     state = argv[4]
-    
+
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name = BINARY \
