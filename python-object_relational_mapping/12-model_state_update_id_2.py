255bc97248a0d961f5771055769cc06589046783
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-11-24 16:45:49 -0500

    updating name state

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N from a database"""
+
+if __name__ == '__main__':
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sys import argv
+    import re
+
+    if (len(argv) != 4):
+        print('Use: username, password, database name and state')
+        exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).where(State.id == 2)\
+            .update({'name': 'New Mexico'})
+    session.commit()
+
+    session.close()
