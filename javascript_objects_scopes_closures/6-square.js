108ea5f56aa241183a29e73a645d251431c20a16
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-12-11 23:57:22 -0500

    fixing code

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -6,6 +6,7 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+	
   charPrint (c) {
     if (c === undefined) c = 'X';
     for (let i = 0; i < this.height; i++) {
