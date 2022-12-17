6ed5dcf0c8e31155ab8d8dfeedce2edb8244d585
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-12-16 23:40:03 -0500

    changing axios

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const { get } = require('axios').default;
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-
-get(url)
-  .then(({ data: { title } }) => console.log(title))
-  .catch((err) => console.log(err));
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
