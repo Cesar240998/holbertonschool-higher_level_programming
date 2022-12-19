a745ee4cddf2fa9a9e1d6fcbd399435b17c76bac
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-12-19 17:07:12 -0500

    task6: computes number of tasks completed

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let newKey;
+    let newValue;
+    const dExp = {};
+    const dIn = JSON.parse(body);
+    for (const dGet of dIn) {
+      if (dGet.completed) {
+        newKey = '' + dGet.userId;
+	if (!dExp[newKey]) {
+	  dExp[newKey] = 1;
+	} else {
+	  newValue = dExp[newKey];
+	  dExp[newKey] = newValue + 1;
+	}
+      }
+    }
+    console.log(dExp);
+  } else {
+    console.log(`Error: ${response.statusCode}`);
+  }
+});
